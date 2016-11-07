6c93f97b91c5366d4873dbe71e877c1f648f26b1
kunalbishnoi <simbishnoi.912@gmail.com>
2016-11-07 15:58:49 +0530

    last changes

--- save_user.php
+++ save_user.php
@@ -7,13 +7,13 @@
    $user = 'root';
    $pass = '';
    $conn = mysql_connect("localhost",$user,$pass,$dbname) or die("Unable to connect: ".mysql_error());
-   
-   $insert_data = "INSERT INTO order_details (username,password,emailid) 
+
+   $insert_data = "INSERT INTO user_details (username,password,emailid) 
                      VALUES ('$insert_name','$insert_pass','$insert_email')";
-      
+
    mysql_select_db($dbname);
    $retval = mysql_query($insert_data,$conn) or die('Unable to insert data: ' . mysql_error());
    echo "Entered data successfully\n";
-   header('Refresh: 2;URL=index.php'); 
+   header('Refresh: 2;URL=index.php');
    mysql_close($conn);
 ?>
